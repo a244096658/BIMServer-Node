f1368ea79a144798d22687414005d498d6388da0
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-05-04 17:40:14 +0300

    cleaning code, beauitify

--- routes/api.js
+++ routes/api.js
@@ -17,47 +17,50 @@ var address = 'http://localhost:8082'
 var client = new BimServerClient(address);
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 // parse application/json
 app.use(bodyParser.json())
 
 
-var AuthInterface={
-  login:function(req,res,next){
+var AuthInterface = {
+    login: function(req, res, next) {
         var username = req.body.username;
         var password = req.body.password;
-        client.login(username,password,function(){
-        var token = client.token
-        console.log(client.token); 
-        res.redirect('/createProject'); // cilent has a property of token. which is assigned value during .login function. 
-        },function(err){
-        console.log(err);
-      })
-
+        client.login(username, password, function() {
+            var token = client.token
+            console.log(client.token);
+            res.redirect('/createProject'); // cilent has a property of token. which is assigned value during .login function.
+            return next()
+        }, function(err) {
+            console.log(err);
+        })
     }
-
-  //more...
-
+    //more...
 };
 
-
-var ServiceInterface ={
-  addProject:function(req,res,next){
-            var projectName =req.body.projectName;
-            var schema=req.body.schema;
-            client.call('ServiceInterface','addProject',{projectName:projectName,schema:schema},function(data){
-                          console.log(data); // the return data from bimsever is json type. 
-                          //give user some response, so user will know they create project ok. 
-                          res.send("ok!");
-                          },function(err){console.log(err)});
-                        },
-
-  getSuggestedDeserializerForExtension:{}
-
-  //more....
-  
+var ServiceInterface = {
+    addProject: function(req, res, next) {
+        var projectName = req.body.projectName;
+        var schema = req.body.schema;
+        client.call('ServiceInterface', 'addProject', {
+            projectName: projectName,
+            schema: schema
+        }, function(data) {
+            console.log(data); // the return data from bimsever is json type.
+            //give user some response, so user will know they create project ok.
+            res.send("ok!");
+            return next()
+        }, function(err) {
+            console.log(err)
+        });
+    },
+    getSuggestedDeserializerForExtension: function(req,res,next) {
+      //Write Function Here
+    }
+    //more....
 };
 
 exports.AuthInterface = AuthInterface;
 exports.ServiceInterface = ServiceInterface;
-
--- server.js
+++ server.js
@@ -10,21 +10,11 @@ var flash = require('connect-flash');
 var app = express();
 var requirejs = require('requirejs');
 var XMLHttpRequest = require("xhr2");
-
-//BIMServer Dependencies
-// var BimServerApiPromise = require('./bimServerJS/bimserverapipromise');
-// var BimServerApiWebSocket = require('./bimServerJS/bimserverapiwebsocket');
 var BimServerClient = require('./bimServerJS/bimserverclient');
-// var ifc2x3 = require('./bimServerJS/ifc2x3tc1');
-// var ifc4 = require('./bimServerJS/ifc4');
-// var Model = require('./bimServerJS/model');
-// var translations = require('./bimServerJS/translations_en');
-
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
-//app.use('/api', expressJwt({secret: 'secret-anne'}));
 app.use(express.json());
 app.use(express.urlencoded());
 //for use with sessions
@@ -36,46 +26,30 @@ app.use(express.session({ cookie: { maxAge: 60000 }, secret: 'anne#'}));
 app.use(flash());
 app.use(app.router);
 app.use(express.static(path.join(__dirname , '/public')));  // set the static files location /public/img will be /img for users
-
-//app.use(express.static({root: path.join(__dirname, './public')}))
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
-// parse application/json
 app.use(bodyParser.json())
-
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-
-//API Main Page
+// GET Requests
 app.get('/', function(req,res,next){
   res.sendfile('./public/index.html');
 });
-
-// API BIMServer Login
 app.get('/login', function(req,res){
   res.sendfile('./public/login.html')
 });
-
-app.post('/login', api.AuthInterface.login);
-
-
-// API BIMServer createproject
 app.get('/createProject', function(req,res){
   res.sendfile('./public/createProject.html');
 });
 
+// GET Requests
+app.post('/login', api.AuthInterface.login);
 app.post('/createProject', api.ServiceInterface.addProject);
-
 
 //Server start
 http.createServer(app).listen(app.get('port'), function(req,res){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-
- 
